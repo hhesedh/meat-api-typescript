6fbf9aff4b5321a28a159b7a03cd457294f9046a
fabio <fabiodeoliveira07@gmail.com>
2019-06-23 17:26:28 -0300

    Alterando um documento via PATCH e exlcuindo via DELETE

--- users/users.router.ts
+++ users/users.router.ts
@@ -29,9 +29,47 @@ class UsersRouter extends Router {
         resp.json(user)
         return next()
       })
-
     })
 
+    application.put('/users/:id', (req, resp, next) => {
+      const options = { overwrite: true }
+      User.update({ _id: req.params.id }, req.body, options)
+        .exec().then(result => {
+          if (result.n) {
+            return User.findById(req.params.id).exec()
+          } else {
+            resp.send(404)
+          }
+        }).then(user => {
+          resp.json(user)
+          return next()
+        })
+    })
+
+    application.patch('/users/:id', (req, resp, next) => {
+      const options = { new: true }
+
+      User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
+        if (user) {
+          resp.json(user)
+          return next()
+        } else {
+          resp.send(404)
+          return next()
+        }
+      })
+    })
+
+    application.del('/users/:id', (req, resp, next) => {
+      User.remove({ _id: req.params.id }).exec().then((cmdResult: any) => {
+        if (cmdResult.result.n) {
+          resp.send(204)
+        } else {
+          resp.send(404)
+        }
+        return next()
+      })
+    })
   }
 
 }
